d6535bd5390fe39a63fa7b1c8e408e0e4ea1727b
Arthur <arthur.vanroost@icloud.com>
2025-03-11 09:41:03 +0100

    questions quizz

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,2 +1,12 @@
 console.log("Bonjour bienvenu sur votre quizz spécial One Piece")
 console.log("On va vous posez une série de 10 questions, a vous de bien y répondre!")
+let question1= "Comment s'appelle le héros principal de One Piece?"
+let question2 ="Quel est le rêve de Monkey D. Luffy?"
+let quesiton3 ="Quel fruit du démon Luffy a-t-il mangé?"
+let question4 ="Comment s'appelle le bateau des Chapeaux de Paille avant le Thousand Sunny?"
+let question5 ="Quel est le nom du sabreur de l'équipage de Luffy?"
+let question6 ="Qui est le médecin de l'équipage des Chapeaux de Paille?"
+let question7 ="Quel est le nom de l'île où se trouve la Marine et les prisons de haute sécurité?"
+let question8 ="Qui est le frère de Luffy qui a mangé le Mera Mera no Mi?"
+let question9 ="Quel est le nom du trésor légendaire recherché par tous les pirates?"
+let question10 ="Qui est le créateur de One Piece?"
